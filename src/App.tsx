3f43967790294dc1121fb86e561d9dbee0f5b7c9
Your Name <u513509754@gmail.com>
2026-02-01 10:59:16 -0700

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { UserWarning } from './UserWarning';
+import { getTodos, USER_ID } from './api/todos';
+import { Filter as Filters, Todo as Todos } from './types/Todo';
 
-const USER_ID = 0;
+import { TodoApp } from './components/TodoApp/todoapp';
+import { Todo } from './components/Todo/todo';
+import { Filter } from './components/Filter/filter';
+import { Index } from './components/index/index';
 
 export const App: React.FC = () => {
+  const [posts, setPosts] = useState<Todos[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [filter, setFilter] = useState<Filters>();
+
+  const [errorMessage, setErrorMessage] = useState('');
+  const hasTodos = posts.length > 0;
+
+  useEffect(() => {
+    setErrorMessage('');
+    setLoading(true);
+    getTodos()
+      .then(setPosts)
+      .catch(() => setErrorMessage('Unable to load todos'))
+      .finally(() => setLoading(false));
+  }, []);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-add-and-delete#react-todo-app-add-and-delete">
-          React Todo App - Add and Delete
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <TodoApp
+          posts={posts}
+          setPosts={setPosts}
+          setErrorMessage={setErrorMessage}
+          loading={loading}
+        />
+
+        {hasTodos && (
+          <>
+            <Todo
+              posts={posts}
+              setErrorMessage={setErrorMessage}
+              setPosts={setPosts}
+              filter={filter}
+            />
+            <Filter
+              setErrorMessage={setErrorMessage}
+              posts={posts}
+              filter={filter}
+              setPosts={setPosts}
+              setFilter={setFilter}
+            />
+          </>
+        )}
+      </div>
+      {!hasTodos && (
+        <Index
+          errorMessage={errorMessage}
+          setLoading={setLoading}
+          setErrorMessage={setErrorMessage}
+          posts={posts}
+        />
+      )}
+    </div>
   );
 };
